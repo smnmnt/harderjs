da83abe881eab16f673524521395cbd2b1d39299
Oleg Smnmnt <o.kosobreev@yandex.ru>
2022-03-16 18:31:09 +0300

    l2 done

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let num = 266219
 num = String(num)
 let numLength = (num.length)
-let result = 0
+let result = 1
+let strDegree
+let degree = 1
 let arr = num.split("")
 
 for (let i = 0; i < numLength; i++)
-  result += Number(num[i])
+  result *= Number(num[i])
 
-console.log(arr);
-console.log(result);
+for (let t = 0; t < 3; t++)
+  degree *= result
+  
+
+console.log(arr)
+console.log(result)
+console.log(degree)
+strDegree = String(degree)
+console.log(strDegree[0], strDegree[1])
